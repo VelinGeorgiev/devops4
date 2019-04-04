8daa705789b3560d0c13502f69964161cf7f7907
Velin Georgiev <velin.georgiev@gmail.com>
2019-04-04 10:19:43 +0100

    init

---
+++ src/webparts/iceCreamShop/test/part5SinonSpiesBasics.test.tsx
@@ -0,0 +1,77 @@
+/// <reference types="jest" />
+
+import * as React from 'react';
+import { configure, mount, ReactWrapper } from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+
+import { IIceCreamShopProps } from '../components/IIceCreamShopProps';
+import { IIceCreamShopState } from '../components/IIceCreamShopState';
+import IceCreamShop from '../components/IceCreamShop';
+import { IceCreamFakeProvider } from '../iceCreamProviders/IceCreamFakeProvider';
+
+import * as sinon from 'sinon';
+
+configure({ adapter: new Adapter() });
+
+describe('Sinon basic spy', () => {
+
+    let reactComponent: ReactWrapper<IIceCreamShopProps, IIceCreamShopState>;
+    let selectHandlerSpy: sinon.SinonSpy;
+
+    beforeEach(() => {
+
+        // set spy on the select handler
+        selectHandlerSpy = sinon.spy(IceCreamShop.prototype, 'selectHandler');
+
+        // mount the component
+        reactComponent = mount(React.createElement(
+            IceCreamShop,
+            {
+                iceCreamProvider: new IceCreamFakeProvider(),
+                strings: {} as IIceCreamShopWebPartStrings
+            }
+        ));
+    });
+
+    afterEach(() => {
+        reactComponent.unmount();
+        selectHandlerSpy.restore();
+    });
+
+    it('should handler be called once', () => {
+
+        reactComponent.update();
+
+        // more advanced selector
+        const selectIceCreamButton = reactComponent.find("#iceCreamFlavoursList button").first();
+
+        selectIceCreamButton.simulate('click'); 
+
+        // after the selectIceCreamButton is clicked   
+        // the buy form should be rendered
+        // lets try to find in in the component
+        const buyForm = reactComponent.find("#buyForm");
+
+        expect(selectHandlerSpy.calledOnce).toBe(true);
+    });
+
+    it('should handler be called with the right parameters', () => {
+
+        reactComponent.update();
+
+        // more advanced selector
+        const selectIceCreamButton = reactComponent.find("#iceCreamFlavoursList button").first();
+
+        selectIceCreamButton.simulate('click');
+
+        // after the selectIceCreamButton is clicked   
+        // the buy form should be rendered
+        // lets try to find in in the component
+        const buyForm = reactComponent.find("#buyForm");
+
+        expect(selectHandlerSpy.lastCall.args[0].UniqueId).toBe('1'); 
+        expect(selectHandlerSpy.lastCall.args[0].Title).toBe('Cherry'); 
+        expect(selectHandlerSpy.lastCall.args[0].Price).toBe(1); 
+    });
+});
+// http://sinonjs.org/
